7eaa8c16aa9cc5a9d4a682ce606a3322d48108b1
Кенан Салахоа <you@example.com>
2022-06-22 12:35:40 +0500

    First commit

--- index.js
+++ index.js
@@ -5,14 +5,11 @@ let fs=require("fs")
 let body=require("body-parser")
 
 let PORT=process.env.PORT || 80
-app.set('view engine','ejs')
-app.set("views", path.resolve(__dirname,"ejs"))
+
 app.listen(PORT,()=>{
     console.log("Server has been launched!")
 })
-app.get("/",(req,res)=>{
-    res.render("index",{text:"Konbanwa!"})
-})
+app.use(express.static(path.resolve(__dirname,"static")))
 
 app.use('/form', body.urlencoded({
     extended: true
@@ -28,48 +25,24 @@ app.post('/form', function(req, res, next) {
     //         console.log("Успешно")
     //     }
     // })
-    filePath=path.join(__dirname,"ejs",req.body.folder_name+".ejs")
+    filePath=path.join(__dirname,"static",req.body.folder_name+".html")
     fs.writeFile(filePath,req.body.text,(err)=>{
    if(err){
        throw err
    }
    else{
        console.log("файл успешно создан")
-       fs.appendFile(path.join(__dirname,"index.js"),`\napp.get("/${req.body.folder_name}",(req,res)=>{
-        res.render("${req.body.folder_name}")
-    })`,(err)=>{
-        if(err){
-            throw err
-        }
-        else{
-            console.log("Успешно")
-        }
-    })
+    //    fs.appendFile(path.join(__dirname,"index.js"),`\napp.get("/${req.body.folder_name}",(req,res)=>{
+    //     res.render("${req.body.folder_name}")
+    // })`,(err)=>{
+    //     if(err){
+    //         throw err
+    //     }
+    //     else{
+    //         console.log("Успешно")
+    //     }
+    // })
    }
     })
 })
 
-app.get("/uma1",(req,res)=>{
-        res.render("uma1")
-    })
-app.get("/uma1",(req,res)=>{
-        res.render("uma1")
-    })
-app.get("/uma2",(req,res)=>{
-        res.render("uma2")
-    })
-app.get("/uma3",(req,res)=>{
-        res.render("uma3")
-    })
-app.get("/uma3",(req,res)=>{
-        res.render("uma3")
-    })
-app.get("/uma7",(req,res)=>{
-        res.render("uma7")
-    })
-app.get("/sd",(req,res)=>{
-        res.render("sd")
-    })
-app.get("/dfdf",(req,res)=>{
-        res.render("dfdf")
-    })
